540ec7ad63605e88dd383ebe083bc2ea9f3c15ca
Kevin Mees <kev.mees@gmail.com>
2018-12-06 19:32:04 +0100

    chore: use storybook-addon-application-insights

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,21 +1,3 @@
 import '@storybook/addon-actions/register'
 import '@storybook/addon-links/register'
-
-import addons from '@storybook/addons'
-import ReactAI from 'react-appinsights'
-
-addons.register('storybook/application-insights', api => {
-  ReactAI.init({ instrumentationKey: '276ac3cd-077a-4595-b82e-24732aa75645' })
-
-  var appInsights = ReactAI.ai()
-
-  api.onStory((kind, story) => {
-    let path = window.location.pathname
-
-    if (path === '/') {
-      path = ''
-    }
-
-    appInsights.trackPageView(story, `${path}/${kind}/${story}`)
-  })
-})
+import 'storybook-addon-application-insights/register'
